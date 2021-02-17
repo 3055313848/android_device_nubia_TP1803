ec405486f22a6b95fb0bba1119509b75756a673a
brian-tango <54069698+brian-tango@users.noreply.github.com>
2021-02-17 03:31:00 -0600

    Update and rename lineage_TP1803.mk to revengeos_TP1803.mk

--- revengeos_TP1803.mk
+++ revengeos_TP1803.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
 
 # Inherit from TP1803 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
