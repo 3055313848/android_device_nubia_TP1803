1e6100c4ec6b840f0d405f9b511b0f3f90e900ca
brian-tango <54069698+brian-tango@users.noreply.github.com>
2021-02-17 03:39:08 -0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/revengeos_TP1803.mk
 COMMON_LUNCH_CHOICES := \
-    lineage_TP1803-user \
-    lineage_TP1803-userdebug \
-    lineage_TP1803-eng
+    revengeos_TP1803-user \
+    revengeos_TP1803-userdebug \
+    revengeos_TP1803-eng
